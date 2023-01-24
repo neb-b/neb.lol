ffbd2b807052e77d101f9092f85dd133157ae1a9
neb <nebb_bb@protonmail.com>
2023-01-24 02:21:09 -0600

    add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/daisy",
+        destination: "/nostr",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
